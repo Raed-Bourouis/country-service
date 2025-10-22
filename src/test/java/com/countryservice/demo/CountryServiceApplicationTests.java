291b91bf5c56d02827b52bd7d69bcfd439079e9c
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2025-10-22 21:04:33 +0100

    country service

---
+++ src/test/java/com/countryservice/demo/CountryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.countryservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		throw new UnsupportedOperationException("To be implemented");
+	}
+
+}

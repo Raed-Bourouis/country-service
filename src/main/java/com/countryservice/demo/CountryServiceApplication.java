2b212f2841306d16b13ae6643b4831b6d9659418
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2025-10-23 10:49:26 +0100

    Docker

--- src/main/java/com/countryservice/demo/CountryServiceApplication.java
+++ src/main/java/com/countryservice/demo/CountryServiceApplication.java
@@ -2,12 +2,18 @@ package com.countryservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryServiceApplication {
+public class CountryserviceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CountryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CountryserviceApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CountryserviceApplication.class);
+    }
 }
